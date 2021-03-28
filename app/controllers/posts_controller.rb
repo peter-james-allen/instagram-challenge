c1133c0efaf408200f32101a511e8c09ceb111dc
Peter Allen <peter_james_allen@hotmail.com>
2021-03-28 19:54:52 +0100

    added sign in authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :logged_in_user, only: [:new, :create]
 
   # GET /posts or /posts.json
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
+
+  def authenticate(password)
+    self.password == password
+  end
+
 end
